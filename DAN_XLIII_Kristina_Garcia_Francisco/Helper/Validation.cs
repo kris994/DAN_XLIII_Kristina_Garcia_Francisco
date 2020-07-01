10b8b929fb279b13753dec93b0aeef50730287a1
kris994 <kristipomo@gmail.com>
2020-07-01 22:14:43 +0200

    Implemented the login function.

---
+++ DAN_XLIII_Kristina_Garcia_Francisco/Helper/Validation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAN_XLIII_Kristina_Garcia_Francisco.Helper
+{
+    class Validation
+    {
+
+    }
+}
