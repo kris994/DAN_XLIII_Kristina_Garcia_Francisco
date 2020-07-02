d06720a62a1e011a2b74771e14a5e911a4560a63
kris994 <kristipomo@gmail.com>
2020-07-02 12:13:59 +0200

    All worker CRUDs added

--- DAN_XLIII_Kristina_Garcia_Francisco/Service.cs
+++ DAN_XLIII_Kristina_Garcia_Francisco/Service.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Windows;
 
 namespace DAN_XLIII_Kristina_Garcia_Francisco
 {
@@ -47,18 +48,18 @@ namespace DAN_XLIII_Kristina_Garcia_Francisco
         {
             try
             {
+                List<tblUser> list = new List<tblUser>();
                 using (ReportDBEntities context = new ReportDBEntities())
                 {
                     for (int i = 0; i < GetAllUsers().Count; i++)
                     {
                         if (GetAllUsers()[i].Access == null)
                         {
-                            List<tblUser> list = new List<tblUser>();
+                            
                             list.Add(GetAllUsers()[i]);
-                            return list;
                         }
                     }
-                    return null;
+                    return list;
                 }
             }
             catch (Exception ex)
@@ -76,18 +77,108 @@ namespace DAN_XLIII_Kristina_Garcia_Francisco
         {
             try
             {
+                List<tblUser> list = new List<tblUser>();
                 using (ReportDBEntities context = new ReportDBEntities())
                 {
                     for (int i = 0; i < GetAllUsers().Count; i++)
                     {
                         if (GetAllUsers()[i].Access != null)
                         {
-                            List<tblUser> list = new List<tblUser>();
                             list.Add(GetAllUsers()[i]);
-                            return list;
+                            
                         }
                     }
-                    return null;
+                    return list;
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Exception" + ex.Message.ToString());
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Gets all information about reports
+        /// </summary>
+        /// <returns>a list of found reports</returns>
+        public List<tblReport> GetAllReports()
+        {
+            try
+            {
+                using (ReportDBEntities context = new ReportDBEntities())
+                {
+                    List<tblReport> list = new List<tblReport>();
+                    list = (from x in context.tblReports select x).ToList();
+                    return list;
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Exception" + ex.Message.ToString());
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Creates or edits a worker
+        /// </summary>
+        /// <param name="worker">the worker that is esing added</param>
+        /// <returns>a new or edited worker</returns>
+        public vwUser AddWorker(vwUser worker)
+        {
+            InputCalculator iv = new InputCalculator();
+            try
+            {
+                using (ReportDBEntities context = new ReportDBEntities())
+                {
+                    if (worker.UserID == 0)
+                    {
+                        worker.DateOfBirth = iv.CountDateOfBirth(worker.JMBG);
+
+                        tblUser newWorker = new tblUser();
+                        newWorker.FirstName = worker.FirstName;
+                        newWorker.LastName = worker.LastName;
+                        newWorker.JMBG = worker.JMBG;
+                        newWorker.DateOfBirth = worker.DateOfBirth;
+                        newWorker.BankAccount = worker.BankAccount;
+                        newWorker.Email = worker.Email;
+                        newWorker.Position = worker.Position;
+                        newWorker.Salary = worker.Salary;
+                        newWorker.Username = worker.Username;
+                        newWorker.UserPassword = worker.UserPassword;
+
+                        context.tblUsers.Add(newWorker);
+                        context.SaveChanges();
+                        worker.UserID = newWorker.UserID;
+                        return worker;
+                    }
+                    else
+                    {
+                        tblUser workerToEdit = (from ss in context.tblUsers where ss.UserID == worker.UserID select ss).First();
+
+                        // Get the date of birth
+                        worker.DateOfBirth = iv.CountDateOfBirth(worker.JMBG);
+
+                        workerToEdit.FirstName = worker.FirstName;
+                        workerToEdit.LastName = worker.LastName;
+                        workerToEdit.JMBG = worker.JMBG;
+                        workerToEdit.DateOfBirth = worker.DateOfBirth;
+                        workerToEdit.BankAccount = worker.BankAccount;
+                        workerToEdit.Email = worker.Email;
+                        workerToEdit.Salary = worker.Salary;
+                        workerToEdit.Username = worker.Username;
+                        workerToEdit.Position = worker.Position;
+                        workerToEdit.UserPassword = worker.UserPassword;
+
+                        workerToEdit.UserID = worker.UserID;
+
+                        tblUser workerEdit = (from ss in context.tblUsers
+                                               where ss.UserID == worker.UserID
+                                               select ss).First();
+                        context.SaveChanges();
+                        return worker;
+                    }
                 }
             }
             catch (Exception ex)
@@ -166,6 +257,80 @@ namespace DAN_XLIII_Kristina_Garcia_Francisco
             {
                 Debug.WriteLine("Exception" + ex.Message.ToString());
                 return null;
+            }
+        }
+
+        /// <summary>
+        /// Search if user with that ID exists in the user table
+        /// </summary>
+        /// <param name="userID">Takes the user id that we want to search for</param>
+        /// <returns>True if the user exists</returns>
+        public bool IsUserID(int userID)
+        {
+            try
+            {
+                using (ReportDBEntities context = new ReportDBEntities())
+                {
+                    int result = (from x in context.tblUsers where x.UserID == userID select x.UserID).FirstOrDefault();
+
+                    if (result != 0)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Exception " + ex.Message.ToString());
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Deletes user, users records and identification card depending if the uderID already exists
+        /// </summary>
+        /// <param name="user">the user that is being deleted</param>
+        /// <returns>list of users</returns>
+        public void DeleteWorker(int userID)
+        {
+            InputCalculator iv = new InputCalculator();
+            List<tblReport> AllRportss = GetAllReports();
+            try
+            {
+                using (ReportDBEntities context = new ReportDBEntities())
+                {
+                    bool isUser = IsUserID(userID);
+                    // Delete are evidences from the user
+                    for (int i = 0; i < AllRportss.Count; i++)
+                    {
+                        if (AllRportss[i].UserID == userID)
+                        {
+                            tblReport report = (from r in context.tblReports where r.UserID == userID select r).First();
+                            context.tblReports.Remove(report);
+                            context.SaveChanges();
+                        }
+                    }
+                    if (isUser == true)
+                    {
+                        // find the user and identification card before removing them
+                        tblUser userToDelete = (from r in context.tblUsers where r.UserID == userID select r).First();
+
+                        context.tblUsers.Remove(userToDelete);
+                        context.SaveChanges();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Cannot delete the user");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Exception" + ex.Message.ToString());
             }
         }
     }
---
+++ DAN_XLIII_Kristina_Garcia_Francisco/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,319 @@
+﻿
+
+using DAN_XLIII_Kristina_Garcia_Francisco.Command;
+using DAN_XLIII_Kristina_Garcia_Francisco.Model;
+using DAN_XLIII_Kristina_Garcia_Francisco.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Input;
+
+namespace DAN_XLIII_Kristina_Garcia_Francisco.ViewModel
+{
+    /// <summary>
+    /// Main Window view model
+    /// </summary>
+    class MainWindowViewModel : BaseViewModel
+    {
+        MainWindow main;
+        Service service = new Service();
+
+        #region Constructor
+        /// <summary>
+        /// Constructor with Main Window param
+        /// </summary>
+        /// <param name="mainOpen">opens the main window</param>
+        public MainWindowViewModel(MainWindow mainOpen)
+        {
+            main = mainOpen;
+            WorkerList = service.GetAllWorkers().ToList();
+        }
+        #endregion
+
+        #region Property
+        /// <summary>
+        /// List of all Workers
+        /// </summary>
+        private List<tblUser> userList;
+        public List<tblUser> UserList
+        {
+            get
+            {
+                return userList;
+            }
+            set
+            {
+                userList = value;
+                OnPropertyChanged("UserList");
+            }
+        }
+
+        /// <summary>
+        /// List of all Workers
+        /// </summary>
+        private List<tblUser> workerList;
+        public List<tblUser> WorkerList
+        {
+            get
+            {
+                return workerList;
+            }
+            set
+            {
+                workerList = value;
+                OnPropertyChanged("WorkerList");
+            }
+        }
+
+        /// <summary>
+        /// Specific User
+        /// </summary>
+        private tblUser user;
+        public tblUser User
+        {
+            get
+            {
+                return user;
+            }
+            set
+            {
+                user = value;
+                OnPropertyChanged("User");
+            }
+        }
+        #endregion
+
+        /// <summary>
+        /// This method searches for the selected Worker
+        /// </summary>
+        /// <returns>the found worker</returns>
+        private vwUser Worker()
+        {
+            try
+            {
+                using (ReportDBEntities db = new ReportDBEntities())
+                {
+                    vwUser user = new vwUser();
+                    user = db.vwUsers.Where(x => x.UserID == User.UserID).First();
+                    return user;
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(ex.Message.ToString());
+                return null;
+            }
+        }
+
+        #region Commands
+        /// <summary>
+        /// Command that tries to delete the worker
+        /// </summary>
+        private ICommand deleteWorker;
+        public ICommand DeleteWorker
+        {
+            get
+            {
+                if (deleteWorker == null)
+                {
+                    deleteWorker = new RelayCommand(param => DeleteWorkerExecute(), param => CanDeleteWorkerExecute());
+                }
+                return deleteWorker;
+            }
+        }
+
+        /// <summary>
+        /// Executes the delete command
+        /// </summary>
+        public void DeleteWorkerExecute()
+        {
+            // Checks if the user really wants to delete the worker
+            var result = MessageBox.Show("Are you sure you want to delete the worker?\nAll worker reports will be deleted as well", "Confirmation", MessageBoxButton.YesNo, MessageBoxImage.Question);
+
+            if (result == MessageBoxResult.Yes)
+            {
+                try
+                {
+                    if (Worker() != null)
+                    {
+                        int userID = Worker().UserID;
+                        service.DeleteWorker(userID);
+                        WorkerList = service.GetAllWorkers().ToList();
+                        UserList = service.GetAllUsers().ToList();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        /// <summary>
+        /// Checks if the worker can be deleted
+        /// </summary>
+        /// <returns>true if possible</returns>
+        public bool CanDeleteWorkerExecute()
+        {
+            if (WorkerList == null)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        /// <summary>
+        /// Command that tries to open the edit worker window
+        /// </summary>
+        private ICommand editWorker;
+        public ICommand EditWorker
+        {
+            get
+            {
+                if (editWorker == null)
+                {
+                    editWorker = new RelayCommand(param => EditWorkerExecute(), param => CanEditWorkerExecute());
+                }
+                return editWorker;
+            }
+        }
+
+        /// <summary>
+        /// Executes the edit command
+        /// </summary>
+        public void EditWorkerExecute()
+        {
+            try
+            {
+                if (Worker() != null)
+                {
+                    AddWorker addWorker = new AddWorker(Worker());
+                    addWorker.ShowDialog();
+
+                    if ((addWorker.DataContext as AddWorkerViewModel).IsUpdateWorker == true)
+                    {
+                        WorkerList = service.GetAllWorkers().ToList();
+                        UserList = service.GetAllUsers().ToList();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        /// <summary>
+        /// Checks if the worker can be edited
+        /// </summary>
+        /// <returns>true if possible</returns>
+        public bool CanEditWorkerExecute()
+        {
+            if (WorkerList == null)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        /// <summary>
+        /// Command that tries to add a new worker
+        /// </summary>
+        private ICommand addNewWorker;
+        public ICommand AddNewWorker
+        {
+            get
+            {
+                if (addNewWorker == null)
+                {
+                    addNewWorker = new RelayCommand(param => AddNewWorkerExecute(), param => CanAddNewWorkerExecute());
+                }
+                return addNewWorker;
+            }
+        }
+
+        /// <summary>
+        /// Executes the add Worker command
+        /// </summary>
+        private void AddNewWorkerExecute()
+        {
+            try
+            {
+                AddWorker addWorker = new AddWorker();
+                addWorker.ShowDialog();
+                if ((addWorker.DataContext as AddWorkerViewModel).IsUpdateWorker == true)
+                {
+                    WorkerList = service.GetAllWorkers().ToList();
+                    UserList = service.GetAllManagers().ToList();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        /// <summary>
+        /// Checks if its possible to add the new Worker
+        /// </summary>
+        /// <returns>true</returns>
+        private bool CanAddNewWorkerExecute()
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// Command that logs off the user
+        /// </summary>
+        private ICommand logoff;
+        public ICommand Logoff
+        {
+            get
+            {
+                if (logoff == null)
+                {
+                    logoff = new RelayCommand(param => LogoffExecute(), param => CanLogoffExecute());
+                }
+                return logoff;
+            }
+        }
+
+        /// <summary>
+        /// Executes the logoff command
+        /// </summary>
+        private void LogoffExecute()
+        {
+            try
+            {
+                Login login = new Login();
+                main.Close();
+                login.Show();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        /// <summary>
+        /// Checks if its possible to logoff
+        /// </summary>
+        /// <returns>true</returns>
+        private bool CanLogoffExecute()
+        {
+            return true;
+        }
+        #endregion
+    }
+}
